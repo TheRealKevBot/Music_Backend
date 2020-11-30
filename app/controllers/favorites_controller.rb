9dea6a8958eeb9865df2bb3694afea53fc076d54
TheRealKevBot <Kglas001@gmail.com>
2020-11-30 15:42:47 -0700

    Favorites BackEnd

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+
+    def index
+        @favorites = Favorite.all 
+        render json: @favorites
+    end
+
+    def create 
+        @favorite = Favorite.create(
+            song_title: params[:song_title],
+            artist_name: params[:artist_name],
+            username: params[:username]
+        )
+        render json: @favorite, status: :created
+    end
+    
 end
